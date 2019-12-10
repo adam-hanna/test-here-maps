31408388c210b4d513a02ebd705ccf6714c3d377
Adam Hanna <ahanna@alumni.mines.edu>
2019-12-10 19:41:24 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+declare global {
+  interface Window {
+    H: any;
+  }
+}
+
+// note: this is added to the window object in index.html
+const { H } = window
+const { REACT_APP_HERE_MAPS_API_KEY } = process.env
+
 const App: React.FC = () => {
+  useEffect(() => {
+    // note: https://developer.here.com/documentation/maps/dev_guide/topics/get-started.html
+    // Initialize the platform object:
+    const platform = new H.service.Platform({
+      'apikey': REACT_APP_HERE_MAPS_API_KEY
+    });
+
+    // Obtain the default map types from the platform object
+    const maptypes = platform.createDefaultLayers();
+
+    // Instantiate (and display) a map object:
+    // tslint:disable-next-line
+    const map = new H.Map(
+      document.getElementById('mapContainer'),
+      maptypes.vector.normal.map,
+      {
+        zoom: 10,
+        center: { lng: 13.4, lat: 52.51 }
+      }
+    );
+  }, []);
+                                                                                                       
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ width: '100vw', height: '100vh' }}>
+      <div id="mapContainer" style={{ width: '100vw', height: '100vh' }} />
     </div>
   );
 }
